2f0d43a71cba98adb84658d6d76e10b68052fe33
Juan Felipe Castillo Cutiva <juan_cutiva10@hotmail.com>
2019-04-11 08:18:56 -0500

    Update main.js
    Se corrige bug de declaración de array.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+var arrayUsers = new Array('administrador','invitado');
+var arrayPasswords = new Array('4dM1n','invitado');
+
 jQuery(document).ready(function() {
-	var arrayUsers = new Array('administrador','invitado');
-	var arrayPasswords = new Array('4dM1n','invitado');
+	
 });
 
 function validateUser() {
