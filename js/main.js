ec1024bcd7739338c2e224642e180b97063dbad3
Juan Felipe Castillo Cutiva <juan_cutiva10@hotmail.com>
2019-04-11 07:58:11 -0500

    Actualización de botón de login
    Se actualiza botón de Ingreso para que tenga función que se parametrizará en main.js y validará a los usuarios que ingresen a la plataforma

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function() {
+	var arrayUsers = new Array('administrador','invitado');
+	var arrayPasswords = new Array('4dM1n','invitado');
+});
+
+function validateUser() {
+	alert('Hola');
+}
