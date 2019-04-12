a0ffc27adef7ded3b745ec0298e657cebd7b9fbd
Juan Felipe Castillo Cutiva <juan_cutiva10@hotmail.com>
2019-04-11 23:48:33 -0500

    Arreglos de bug
    Arreglos de bugs, ahora funciona correctamente

--- views/accesit.php
+++ views/accesit.php
@@ -1,9 +1,9 @@
 <?php
-include '/juancutiva.github.io/parts/header.html';
+include '../parts/header.html';
 ?>
 <div>
 	Holaaa estas en la nueva pagina
 </div>
 <?php
-include '/juancutiva.github.io/parts/footer.html';
+include '../parts/footer.html';
 ?>
